e14a1b6acfef41d1fe780339586c42fcf8ef8354
Andrey Nikolaev <anikolaev@virtuozzo.com>
2023-03-15 23:20:10 +0200

    added openstack credentials template file

---
+++ openstack-creds.sh
@@ -0,0 +1,14 @@
+export OS_PROJECT_DOMAIN_NAME=student1          # replace "student1" with your project name
+export OS_USER_DOMAIN_NAME=vhi-ops              # replace "vhi-ops" with your domain name
+export OS_PROJECT_NAME=student1                 # replace "student1" with your project name again
+export OS_USERNAME=student1                     # replace "student1" with your user name
+export OS_PASSWORD=MyPassword                   # replace "MyPassword" with password of your user
+export OS_AUTH_URL=https://mycloud.com:5000/v3  # replace "mycloud.com" with the domain name of your self-service panel
+export OS_IDENTITY_API_VERSION=3
+export OS_AUTH_TYPE=password
+export OS_INSECURE=true
+export PYTHONWARNINGS="ignore:Unverified HTTPS request is being made"
+export NOVACLIENT_INSECURE=true
+export NEUTRONCLIENT_INSECURE=true
+export CINDERCLIENT_INSECURE=true
+export OS_PLACEMENT_API_VERSION=1.22
