602b50d42b8d38baeecf5d47d632f97677faa020
Andrey Nikolaev <anikolaev@virtuozzo.com>
2023-11-21 15:28:36 +0200

    Switch to modern Terraform resource referencing

--- 20_res_bastion.tf
+++ 20_res_bastion.tf
@@ -15,7 +15,7 @@ resource "openstack_compute_instance_v2" "bastion" {
   user_data = file("cloud-init/bastion.sh")
   
   network {
-    name = "${openstack_networking_network_v2.lab-public.name}"
+    name = openstack_networking_network_v2.lab-public.name
     fixed_ip_v4 = "10.0.102.250"
   }
 }
@@ -25,7 +25,7 @@ resource "openstack_networking_floatingip_v2" "bastion_float" {
 }
 
 resource "openstack_compute_floatingip_associate_v2" "bastion_float" {
-  floating_ip = "${openstack_networking_floatingip_v2.bastion_float.address}"
-  instance_id = "${openstack_compute_instance_v2.bastion.id}"
-  fixed_ip    = "${openstack_compute_instance_v2.bastion.network.0.fixed_ip_v4}"
+  floating_ip = openstack_networking_floatingip_v2.bastion_float.address
+  instance_id = openstack_compute_instance_v2.bastion.id
+  fixed_ip    = openstack_compute_instance_v2.bastion.network[0].fixed_ip_v4
 }
