552a7ea593da41dd92b6b0fa37d5a1baf5817719
vz-akochkov <alexander.kochkov@virtuozzo.com>
2025-06-20 14:54:47 +0200

    Hotfix for image name parsing

--- 10_data_vhi_cluster.tf
+++ 10_data_vhi_cluster.tf
@@ -1,12 +1,13 @@
 ## VHI instances data
 # Image lookup only when using name
 data "openstack_images_image_v2" "vhi-image" {
-  count = var.vhi-image_isUUID ? 1 : 0
-  name  = var.vhi-image
+  count        = var.vhi-image_isUUID ? 0 : 1
+  name         = var.vhi-image
+  most_recent  = true
 }
 # Get the actual image ID based on the mode
 locals {
-  vhi-image_id = var.vhi-image_isUUID ? data.openstack_images_image_v2.vhi-image[0].id : var.vhi-image
+  vhi-image_id = var.vhi-image_isUUID ? var.vhi-image : data.openstack_images_image_v2.vhi-image[0].id
 }
 
 data "openstack_compute_flavor_v2" "vhi-flavor_main" {
